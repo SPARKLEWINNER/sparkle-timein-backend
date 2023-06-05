861e53bdbcfa3fdde7b403b08059c3d4b6499af0
Edru <edrugonzales@gmail.com>
2023-06-05 20:27:02 +0800

    Remove cron on other server

--- src/index.js
+++ src/index.js
@@ -61,43 +61,7 @@ io.on("connection", (_socket) => {
 });
 
 // Run cronjob
-cron.schedule('*/5 * * * *', () => {
-  fetch("https://api.heroku.com/apps/sparkle-time-keep/dynos", {
-    method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/vnd.heroku+json; version=3',
-      'Authorization': 'Bearer 1a7ca021-0b51-4d98-b188-c7240a9b3504'
-    },
-  })
-    .then((response) => {
-      console.log("Restart Dyno Success")
-    })
-    .catch(function (err) {
-      console.log("Unable to fetch -", err);
-    });
-});
-  
 
-cron.schedule('0 0 */3 * * *', () => {
-  fetch("https://time-in-production-api.onrender.com/api/user/recordsv2/63da3b35ea1925002f719ee1/2023-01-16/2023-01-31", {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/vnd.heroku+json; version=3',
-    },
-  })
-    .then((response) => {
-      console.log("Request Success")
-    })
-    .catch(function (err) {
-      console.log("Unable to fetch -", err);
-    });
-});
 
 function cronTimein (id, location) {
 
