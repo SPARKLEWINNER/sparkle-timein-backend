3a651bf4a8d52345bca78c7763e838e7c44af96d
pixelDigitTony <tonyboi122450@gmail.com>
2024-09-10 09:44:43 +0800

    feat: implemented scheduled web push cron job logic

--- src/services/notif.js
+++ src/services/notif.js
@@ -2,6 +2,7 @@
 const {messaging} = require('./firebase');
 const FCMTOKEN = require('../models/Fcmtokens')
 const Schedule = require('../models/Schedule')
+const cron = require('node-cron');
 require('dotenv').config()
 
 const sendNotification = async (token, message) => {
@@ -22,7 +23,7 @@ const sendNotification = async (token, message) => {
 };
 
 
-const checkTimeForUsers = async () => {
+const checkTimeForSubscribedUsers = async () => {
   try {
     const now = new Date();
     const fcmDet = await FCMTOKEN.find({device: 'pwa'});
@@ -32,11 +33,16 @@ const checkTimeForUsers = async () => {
     }
 
     const messages = fcmDet.map((det) => {
-       return Schedule.findOne({uid: `${det.userId}` , date: new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()))})
+      const date = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()));
+      const formattedDate = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), 0, 0, 0));
+      return Schedule.findOne({uid: `${det.userId}` , date: formattedDate}).then(schedule => ({
+        fcmToken: det.fcmToken,
+        schedule
+      }))
     });
 
-    console.log('ito oh:', messages)
-
+    const result = await Promise.all(messages)
+    return result.filter(r => r.schedule !== null)
     
   } catch (err) {
     console.error('Error checking time for user:', err);
@@ -44,17 +50,69 @@ const checkTimeForUsers = async () => {
 };
 
 
-// const watchMultipleUsersEventTime = () => {
-//   setInterval(async () => {
-//       await checkTimeForUsers();
-//   }, 300000); // Check every minute
-// };
-
 const watchMultipleUsersEventTime = () => {
-  setInterval(async () => {
-      await checkTimeForUsers();
+  setInterval( () => {
+       checkFiveMinutesAfter();
   }, 5000); // Check every minute
 };
 
 // Start watching for multiple users
 // watchMultipleUsersEventTime();
+
+const checkFiveMinutesAfter = () => {
+  console.log('Checking 5 minutes after every hour');
+  cron.schedule('05 * * * *', async () => {
+    try {
+      const result = await checkTimeForSubscribedUsers();
+      if (result) {
+        result.forEach((data) => {
+          console.log('data: ', data)
+          const {fcmToken, schedule} = data
+          if (schedule) {
+            const {to} = schedule
+            const message = `Your time is past ${to}, Please time out soon.`
+            const currentTime = new Date()
+            const scheduleTime = new Date(to)
+            scheduleTime.setMinutes(scheduleTime.getMinutes() + 5)
+            if(scheduleTime === currentTime && new Date() > new Date(to)){
+              sendNotification(fcmToken, message);
+            }
+          }
+        })
+      }
+    } catch (e) {
+      console.error('Error checking time for user:', e);
+    }
+  });
+}
+
+checkFiveMinutesAfter();
+const checkFiveMinutesBefore = () => {
+  console.log('Checking 5 minutes before every hour');
+  cron.schedule('55 * * * *', async () => {
+    try {
+      const result = await checkTimeForSubscribedUsers();
+      if (result) {
+        result.forEach((data) => {
+          console.log('data: ', data)
+          const {fcmToken, schedule} = data
+          if (schedule) {
+            const {from} = schedule
+            const message = `Your time ${from} is nearing, Please time in soon.`
+            const currentTime = new Date()
+            const scheduleTime = new Date(from)
+            scheduleTime.setMinutes(scheduleTime.getMinutes() - 5)
+            if(scheduleTime === currentTime && new Date() < new Date(from)){
+              sendNotification(fcmToken, message);
+            }
+          }
+        })
+      }
+    } catch (e) {
+      console.error('Error checking time for user:', e);
+    }
+  });
+}
+
+
+checkFiveMinutesBefore();
